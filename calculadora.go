6f5604041d17c5ef6981accaec5a0a7b6b9ae73b
Max Dron <maxdron@MacBook-Pro-de-Max.local>
2023-04-24 18:01:08 +0200

    comienzo del proyecto

---
+++ calculadora.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var numero1 int
+
+	var numero2 int
+
+	fmt.Println("Dame un numero que te lo sumo")
+
+	fmt.Scan(&numero1)
+
+	fmt.Println("Ahora dame el segundo numero")
+
+	fmt.Scan(&numero2)
+
+	fmt.Println("Ahi va el resultado", numero1 + numero2)
+
+
+	
+} 
+
+
+
